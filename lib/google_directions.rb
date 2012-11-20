a1bf4381cae08892da290bf8bd64e0b2038c84dd
svs <svs@svs.io>
2012-11-20 06:56:21 +0530

    changed xml to json and added further parameters to request

--- lib/google_directions.rb
+++ lib/google_directions.rb
@@ -4,11 +4,11 @@ require 'google_directions'
 
 class GoogleDirections
   
-  def initialize(location_1, location_2)
-    @base_url = "http://maps.google.com/maps/api/directions/xml?key=#{GOOGLE_MAPS_API_KEY}&sensor=false&"
+  def initialize(location_1, location_2, options = "")
+    @base_url = "http://maps.google.com/maps/api/directions/json?sensor=false&"
     @location_1 = location_1
     @location_2 = location_2
-    options = "origin=#{transcribe(@location_1)}&destination=#{transcribe(@location_2)}"
+    options = "origin=#{transcribe(@location_1)}&destination=#{transcribe(@location_2)}&#{options}"
     @xml_call = @base_url + options
     @status = find_status
   end
@@ -69,7 +69,7 @@ class GoogleDirections
     end
 
     def get_url(url)
-      Net::HTTP.get(::URI.parse(url))
+      Net::HTTP.get(::URI.parse(::URI.encode(url)))
     end
   
 end
